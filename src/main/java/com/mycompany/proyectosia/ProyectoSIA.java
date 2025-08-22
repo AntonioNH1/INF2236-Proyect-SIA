897517438e202d5e678a28bd320d4480a190506e
anton <anton@AntonioX>
2025-08-22 17:07:56 -0400

    prueba1

--- src/main/java/com/mycompany/proyectosia/ProyectoSIA.java
+++ src/main/java/com/mycompany/proyectosia/ProyectoSIA.java
@@ -10,6 +10,7 @@ package com.mycompany.proyectosia;
  */
 public class ProyectoSIA {
 
+    int aaa;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
