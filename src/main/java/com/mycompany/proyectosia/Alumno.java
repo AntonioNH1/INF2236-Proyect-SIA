252fb921c72696b6f7803b4f3aab5c3fb7dfec4b
anton <anton@AntonioX>
2025-08-25 22:26:14 -0400

    prueba1

--- src/main/java/com/mycompany/proyectosia/Alumno.java
+++ src/main/java/com/mycompany/proyectosia/Alumno.java
@@ -8,6 +8,26 @@ package com.mycompany.proyectosia;
  *
  * @author anton
  */
+package com.mycompany.proyectosia;
+
 public class Alumno {
-    
+    private String nombre;
+    private String curso;
+
+    public Alumno(String nombre, String curso) {
+        this.nombre = nombre;
+        this.curso = curso;
+    }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getCurso() { return curso; }
+    public void setCurso(String curso) { this.curso = curso; }
+
+    // Puedes borrar este método si no quieres usar toString
+    @Override
+    public String toString() {
+        return "Alumno{nombre='" + nombre + "', curso='" + curso + "'}";
+    }
 }
