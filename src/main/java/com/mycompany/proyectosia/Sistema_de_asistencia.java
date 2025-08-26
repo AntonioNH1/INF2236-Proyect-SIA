de1aa45ee2f7809e143643d598d2e678955da9f7
anton <anton@AntonioX>
2025-08-25 22:25:24 -0400

    prueba1

--- src/main/java/com/mycompany/proyectosia/Sistema_de_asistencia.java
+++ src/main/java/com/mycompany/proyectosia/Sistema_de_asistencia.java
@@ -11,36 +11,9 @@ import java.io.*;
  * @author anton
  */
 public class Main {
-
-    // Colección genérica sin tipo específico
-    static ArrayList listaAlumnos = new ArrayList();
-
-    static BufferedReader lectorConsola = new BufferedReader(new InputStreamReader(System.in));
-
     public static void main(String[] args) throws IOException {
-        int opcionMenu;
-
-        do {
-            System.out.println("\n=== Sistema de Asistencia ===");
-            System.out.println("1. Insertar alumno");
-            System.out.println("2. Mostrar alumnos");
-            System.out.println("3. Salir");
-            System.out.print("Elige una opcion: ");
-
-            opcionMenu = leerEnteroSeguro();
-
-            switch (opcionMenu) {
-                case 1:
-                    insertarAlumno();
-                    break;
-                case 2:
-                    mostrarAlumnos();
-                    break;
-                case 3:
-                    System.out.println("Saliendo del sistema...");
-                    break;
-                default:
-                    System.out.println("Opcion invalida.");
-            }
-        } while (opcionMenu != 3);
+        SistemaAsistenciaEscolar sistema = new SistemaAsistenciaEscolar();
+        sistema.iniciar();
     }
+}
+
